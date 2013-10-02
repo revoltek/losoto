2671166bc8bbdbf20cdfb707ac254dadbb2c2c83
revoltek <rev@voo.it>
2013-10-02 11:18:09 +0200

    tests added

--- tools/losoto_test.py
+++ tools/losoto_test.py
@@ -39,6 +39,7 @@ parmdbFile = options.parmdb
 P = lofar.parmdb.parmdb(parmdbFile)
 logging.info("parmdb filename = "+parmdbFile)
 
+######################################################
 # read frequencies
 logging.info("### Read all frequencies for a pol/dir/station")
 
@@ -54,6 +55,7 @@ logging.info("H5parm -- "+str(elapsed)+" s.")
 
 print "Equal?",(Pfreqs == Hfreqs).all()
 
+######################################################
 # read times
 logging.info("### Read all times for a pol/dir/station")
 
@@ -71,65 +73,43 @@ logging.info("H5parm -- "+str(elapsed)+" s.")
 print Htimes
 print "Equal?",(Ptimes == Htimes).all()
 
-# read amp solutions
-logging.info("### Read all amplitudes for a pol/dir/station")
+######################################################
+## read amp solutions
+#logging.info("### Read all amplitudes for a pol/dir/station")
+#
+#start = time.clock()
+#Preal = P.getValuesGrid('DirectionalGain:0:0:Real:CS001LBA:3C196')['DirectionalGain:0:0:Real:CS001LBA:3C196']['values']
+#Pim = P.getValuesGrid('DirectionalGain:0:0:Imag:CS001LBA:3C196')['DirectionalGain:0:0:Imag:CS001LBA:3C196']['values']
+#Pamp = np.sqrt((Preal**2)+(Pim**2)).transpose()[0]
+#elapsed = (time.clock() - start)
+#logging.info("PARMDB -- "+str(elapsed)+" s.")
+#
+#start = time.clock()
+#Hamp = H.val
+#elapsed = (time.clock() - start)
+#logging.info("H5parm -- "+str(elapsed)+" s.")
+#
+#print "Equal?", (Pamp == Hamp).all()
+#
+######################################################
+## read real solutions
+#logging.info("### Read all real for a pol/dir/station")
+#
+#start = time.clock()
+#Preal = P.getValuesGrid('DirectionalGain:0:0:Real:CS001LBA:3C196')['DirectionalGain:0:0:Real:CS001LBA:3C196']['values'].transpose()[0]
+#elapsed = (time.clock() - start)
+#logging.info("PARMDB -- "+str(elapsed)+" s.")
+#
+#start = time.clock()
+#Hamp = H.val
+#Hphase = H2.val
+#Hreal = (Hamp * (np.cos(Hphase) + np.sin(Hphase)*1j)).real
+#elapsed = (time.clock() - start)
+#logging.info("H5parm -- "+str(elapsed)+" s.")
+#
+#print "Equal?", (Preal == Hreal).all()
 
-start = time.clock()
-Preal = P.getValuesGrid('DirectionalGain:0:0:Real:CS001LBA:3C196')['DirectionalGain:0:0:Real:CS001LBA:3C196']['values']
-Pim = P.getValuesGrid('DirectionalGain:0:0:Imag:CS001LBA:3C196')['DirectionalGain:0:0:Imag:CS001LBA:3C196']['values']
-Pamp = np.sqrt((Preal**2)+(Pim**2)).transpose()[0]
-elapsed = (time.clock() - start)
-logging.info("PARMDB -- "+str(elapsed)+" s.")
-
-start = time.clock()
-Hamp = H.val
-elapsed = (time.clock() - start)
-logging.info("H5parm -- "+str(elapsed)+" s.")
-
-print "Equal?", (Pamp == Hamp).all()
-
-# read real solutions
-logging.info("### Read all real for a pol/dir/station")
-
-start = time.clock()
-Preal = P.getValuesGrid('DirectionalGain:0:0:Real:CS001LBA:3C196')['DirectionalGain:0:0:Real:CS001LBA:3C196']['values'].transpose()[0]
-elapsed = (time.clock() - start)
-logging.info("PARMDB -- "+str(elapsed)+" s.")
-
-start = time.clock()
-Hamp = H.val
-Hphase = H2.val
-Hreal = (Hamp * (np.cos(Hphase) + np.sin(Hphase)*1j)).real
-elapsed = (time.clock() - start)
-logging.info("H5parm -- "+str(elapsed)+" s.")
-
-print "Equal?", (Preal == Hreal).all()
-
-# read rotation solutions (== no conversions)
-logging.info("### Read all rotations for a dir/station")
-
-start = time.clock()
-Prot = P.getValuesGrid('CommonRotationAngle:CS001LBA')['CommonRotationAngle:CS001LBA']['values'].transpose()[0]
-elapsed = (time.clock() - start)
-logging.info("PARMDB -- "+str(elapsed)+" s.")
-
-H = solFetcher(H5.getSoltab(solset,'rotation000'))
-H.makeSelection(ant='CS001LBA', dir='pointing')
-
-start = time.clock()
-Hrot = H.val[:,0,0,0]
-elapsed = (time.clock() - start)
-logging.info("H5parm -- "+str(elapsed)+" s.")
-
-print "Equal?", (Prot == Hrot).all()
-
-start = time.clock()
-Hrot2 = np.array( [x['val'] for x in H.getRowsIterator()] )
-elapsed = (time.clock() - start)
-logging.info("H5parm2 -- "+str(elapsed)+" s.")
-
-print "Equal?", (Prot == Hrot2).all()
-
+######################################################
 # read a slice in time
 logging.info("### Read all rotations for a dir/station (slice in time)")
 
@@ -145,11 +125,67 @@ logging.info("H5parm -- "+str(elapsed)+" s.")
 
 print "Equal?", (Prot == Hrot).all()
 
+######################################################
+# read rotation solutions (== no conversions)
+logging.info("### Read all rotations for a dir/station")
 
+start = time.clock()
+Prot = P.getValuesGrid('CommonRotationAngle:CS001LBA')['CommonRotationAngle:CS001LBA']['values'].transpose()[0]
+elapsed = (time.clock() - start)
+logging.info("PARMDB -- "+str(elapsed)+" s.")
+
+H = solFetcher(H5.getSoltab(solset,'rotation000'))
+H.makeSelection(ant='CS001LBA', dir='pointing')
+
+start = time.clock()
+Hrot = H.val[:,0,0,0]
+elapsed = (time.clock() - start)
+logging.info("H5parm H.val -- "+str(elapsed)+" s.")
+
+print "Equal?", (Prot == Hrot).all()
+
+start = time.clock()
+Hrot2 = np.array( [x['val'] for x in H.getRowsIterator()] )
+elapsed = (time.clock() - start)
+logging.info("H5parm getRowsIterator() -- "+str(elapsed)+" s.")
+
+print "Equal?", (Prot == Hrot2).all()
+
+start = time.clock()
+Hrot3, axes = H.getValuesGrid()
+elapsed = (time.clock() - start)
+logging.info("H5parm getValuesGrid() -- "+str(elapsed)+" s.")
+
+print "Equal?", (Prot == Hrot3).all()
+
+start = time.clock()
+for val, axes in H.getIterValuesGrid():
+    pass
+elapsed = (time.clock() - start)
+logging.info("H5parm getIterValuesGrid() -- "+str(elapsed)+" s.")
+
+######################################################
 # read whole file
 logging.info("### Read and tabulate the whole file")
 start = time.clock()
-val = H.getValuesGrid('')
+val, axes = H.getValuesGrid('')
+elapsed = (time.clock() - start)
+logging.info("H5parm -- "+str(elapsed)+" s.")
+
+######################################################
+# read+write
+logging.info("### Read all rotations for a dir/station and write them back")
+Hw = solWriter(H5.getSoltab(solset,'amplitude000'))
+
+start = time.clock()
+Prot = P.getValues('CommonRotationAngle:CS001LBA')['CommonRotationAngle:CS001LBA']['values']
+# parmdb write?
+elapsed = (time.clock() - start)
+logging.info("PARMDB -- "+str(elapsed)+" s.")
+
+start = time.clock()
+Hrot, axes, nrows = H.getValuesGrid(return_nrows = True)
+Hw.setValueGrid(Hrot, nrows)
 elapsed = (time.clock() - start)
 logging.info("H5parm -- "+str(elapsed)+" s.")
 
