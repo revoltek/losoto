767d918b6bef03df679f61c3490bcce3627ee9a6
Francesco de Gasperin <fdg@lofar.eu>
2013-10-30 16:15:37 +0000

    bug in rescale

--- operations/interp.py
+++ operations/interp.py
@@ -38,59 +38,70 @@ def run( step, parset, H ):
         logging.error('A medAxis is needed for rescaling.')
         return 1
 
+    # open calibration table
+    css, cst = calSoltab.split('/')
+    cr = solFetcher(H.getSoltab(css, cst))
+    cAxesNames = cr.getAxesNames()
+
     for soltab in openSoltabs( H, soltabs ):
         logging.info("--> Working on soltab: "+soltab._v_name)
 
         tr = solFetcher(soltab)
         tw = solWriter(soltab)
-        css, cst = calSoltab.split('/')
-        cr = solFetcher(H.getSoltab(css, cst))
 
         axesNames = tr.getAxesNames()
         for i, interpAxis in enumerate(interpAxes[:]):
-            if interpAxis not in axesNames:
+            if interpAxis not in axesNames or interpAxis not in cAxesNames:
                 logging.error('Axis '+interpAxis+' not found. Ignoring.')
                 del interpAxes[i]
         for i, medAxis in enumerate(medAxes[:]):
-            if medAxis not in axesNames:
+            if medAxis not in axesNames or medAxis not in cAxesNames:
                 logging.error('Axis '+medAxis+' not found. Ignoring.')
                 del medAxes[i]
 
         tr.setSelection(ant=ants, pol=pols, dir=dirs)
         for vals, coord in tr.getValuesIter(returnAxes=interpAxes):
 
-            # constract grid
+            # construct grid
             coordSel = removeKeys(coord, interpAxes)
             logging.debug("Working on coords:"+str(coordSel))
             cr.setSelection(**coordSel)
             calValues, calCoord = cr.getValues()
+            # fill all medAxes with the median value
             for medAxis in medAxes:
-                axis = cr.getAxesNames().index(medAxis)
+                axis = cAxesNames.index(medAxis)
                 calValues = np.repeat( np.expand_dims( np.median( calValues, axis ), axis ), calValues.shape[axis], axis )
               
             # create a list of values whose coords are calPoints
+            logging.debug("Flatten arrays.")
             calValues = np.ndarray.flatten(calValues)
 
             # create calibrator/target coordinates arrays
+            logging.debug("Get axes.")
             calPoints = []
             targetPoints = []
             for interpAxis in interpAxes:
                 calPoints.append(calCoord[interpAxis])
                 targetPoints.append(coord[interpAxis])
+            logging.debug("cal product.")
             calPoints = np.array([x for x in itertools.product(*calPoints)])
+            logging.debug("tgt product.")
             targetPoints = np.array([x for x in itertools.product(*targetPoints)])
 
             # interpolation
+            logging.debug("Interpolate.")
             valsNew = scipy.interpolate.griddata(calPoints, calValues, targetPoints, interpMethod)
+
             # fill values outside boudaries with "nearest" solutions
             if interpMethod != 'nearest':
                 valsNewNearest = scipy.interpolate.griddata(calPoints, calValues, targetPoints, 'nearest')
                 valsNew[ np.where(valsNew == np.nan) ] = valsNewNearest [ np.where(valsNew == np.nan) ]
             valsNew = valsNew.reshape(vals.shape)
+
             if rescale:
                 # rescale solutions
                 for medAxis in medAxes:
-                    axis = tr.getAxesNames().index(medAxis)
+                    axis = interpAxes.index(medAxis)
                     valsMed = np.repeat( np.expand_dims( np.median( vals, axis ), axis ), vals.shape[axis], axis )
                     valsNewMed = np.repeat( np.expand_dims( np.median( valsNew, axis ), axis ), valsNew.shape[axis], axis )
                 valsNew = vals*valsNewMed/valsMed
