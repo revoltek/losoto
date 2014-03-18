4ef0ee07225b56d6107b44cb65d6d9c3c7efe621
Martin Hardcastle <mjh@extragalactic.info>
2014-03-18 16:37:10 +0000

    pytables 3.1.0 compatibility

--- losoto.py
+++ losoto.py
@@ -27,10 +27,17 @@ def my_close_open_files(verbose):
     are_open_files = len(open_files) > 0
     if verbose and are_open_files:
         print >> sys.stderr, "Closing remaining open files:",
-    for fileh in open_files.keys():
-        if verbose:
-            print >> sys.stderr, "%s..." % (open_files[fileh].filename,),
-        open_files[fileh].close()
+    # Compatibility fix
+    if tables.__version__>='3.1.0':
+        for fileh in list(open_files.handlers):
+            if verbose:
+                print >> sys.stderr, "%s..." % (fileh.filename,),
+            fileh.close()
+    else:
+        for fileh in open_files.keys():
+            if verbose:
+                print >> sys.stderr, "%s..." % (open_files[fileh].filename,),
+            open_files[fileh].close()
         if verbose:
             print >> sys.stderr, "done",
     if verbose and are_open_files:
