fix wrong axis values for pol

operations/clocktec.py
@@ -21,13 +21,6 @@ def run( step, parset, H ):
     import numpy as np
     from h5parm import solFetcher, solWriter
 
-#    ants = getParAxis( step, parset, H, 'ant' )
-#    logging.info('Ant: '+str(ants))
-#    pols = getParAxis( step, parset, H, 'pol' )
-#    logging.info('Pol: '+str(pols))
-#    dirs = getParAxis( step, parset, H, 'dir' )
-#    logging.info('Dir: '+str(dirs))
-
     # get involved solsets using local step values or global values or all
     solsets = getParSolsets( step, parset, H )
     logging.info('Solset: '+str(solsets))
@@ -50,8 +43,7 @@ def run( step, parset, H ):
 
         solType = t.getType()
         if solType != 'phase':
-           
-           logging.info("Soltab type of "+soltab._v_name+" is: "+solType," should be phase")
+           logging.warning("Soltab type of "+soltab._v_name+" is: "+solType+" should be phase. Ignoring.")
            continue
 
         # this will make a selection for the getValues() and getValuesIter()
@@ -80,26 +72,27 @@ def run( step, parset, H ):
             axes=[i for i in names if i in returnAxes]
             clock,tec,offset,newstations=doFit(ph,freqs,stations,station_positions,axes)
             times=coord['time']
-            tf_st = H.makeSoltab(solsetname, 'tec', 'tec',
-                                 axesNames=['time', 'ant','pol'], axesVals=[times, newstations,np.arange(2)],
+            tf_st = H.makeSoltab(solsetname, 'tec',
+                                 axesNames=['time', 'ant','pol'], axesVals=[times, newstations, ['XX','YY']],
                                  vals=tec,
                                  weights=np.ones_like(tec))
             sw = solWriter(tf_st)
             sw.addHistory('CREATE (by CLOCKTECFIT operation)')
-            tf_st = H.makeSoltab(solsetname, 'clock', 'clock',
-                                 axesNames=['time', 'ant','pol'], axesVals=[times, newstations,np.arange(2)],
+            tf_st = H.makeSoltab(solsetname, 'clock',
+                                 axesNames=['time', 'ant','pol'], axesVals=[times, newstations, ['XX','YY']],
                                  vals=clock,
                                  weights=np.ones_like(clock))
             sw = solWriter(tf_st)
             sw.addHistory('CREATE (by CLOCKTECFIT operation)')
-            tf_st = H.makeSoltab(solsetname, 'offset', 'phase_offset',
-                                 axesNames=['ant','pol'], axesVals=[newstations,np.arange(2)],
+            tf_st = H.makeSoltab(solsetname, 'phase_offset',
+                                 axesNames=['ant','pol'], axesVals=[newstations, ['XX','YY']],
                                  vals=offset,
                                  weights=np.ones_like(offset))
             sw = solWriter(tf_st)
             sw.addHistory('CREATE (by CLOCKTECFIT operation)')
 
     # Add history
+    sw.addHistory('Run CLOCKTECFIT operation')
     
     return 0 # if everything went fine, otherwise 1
 