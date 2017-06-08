6b7021d691a3e6c07dfdb57ae0072f32afffa966
revoltek <rev@voo.it>
2017-06-08 12:25:32 +0000

    update new cep3

--- tools/lofarinit.sh
+++ tools/lofarinit.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # optimized for cep3
 if [ -z $PYTHONPATH ]; then export $PYTHONPATH="" ; fi
-export PYTHONPATH="/home/fdg/scripts/:/home/fdg/opt/lib/python:/home/fdg/opt/lib/python2.6/site-packages:/usr/lib/python2.6/site-packages:/home/fdg/opt/lib/python2.7/site-packages:/usr/lib/python2.7/site-packages:/home/fdg/.local/lib/python2.7/site-packages:/home/fdg/losoto:/home/fdg/scripts/:/home/fdg/opt/lib/python:/home/fdg/opt/lib/python2.6/site-packages:/usr/lib/python2.6/site-packages:/home/fdg/opt/lib/python2.7/site-packages:/usr/lib/python2.7/site-packages:/home/fdg/.local/lib/python2.7/site-packages:/opt/cep/tools/scripts//scripts-master:/opt/cep/LofIm/daily/Thu/lofar_build/install/gnu_opt/lib/python2.7/site-packages:/opt/cep/pyrap/current/lib/python2.7/site-packages:/opt/cep/lofar/external/lib/python/site-packages:/opt/cep/tools/cookbook:/opt/cep/tools/drawMS:/opt/cep/tools/uvplot:/opt/cep/tools/simulation:/opt/cep/tools/lib/python2.7/site-packages:${PYTHONPATH}"
-export PATH="${PATH}:/home/fdg/losoto/bin"
+export PYTHONPATH=":/home/fdg/losoto:$PYTHONPATH"
+export PATH="/home/fdg/losoto/bin:$PATH"
