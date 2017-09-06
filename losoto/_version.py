1d26695ea12aa913c6a9ad44add5971c14ee1eff
revoltek <rev@voo.it>
2017-09-06 19:21:31 +0200

    version

--- losoto/_version.py
+++ losoto/_version.py
@@ -4,8 +4,7 @@ This module simply stores the version
 """
 
 # Version number
-__version__ = '0.6'
-__revision__ = filter(str.isdigit, "$Revision: 00000 $")
+__version__ = '2.0'
 
 # H5parm version
 __h5parmVersion__ = '1.0'
