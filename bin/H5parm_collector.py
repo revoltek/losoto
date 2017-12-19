3738645fa493154195757c9de9575a9ffe87acb3
Francesco Gasperin <rev@voo.it>
2017-12-19 19:15:11 +0100

    simplification

--- bin/H5parm_collector.py
+++ bin/H5parm_collector.py
@@ -19,12 +19,9 @@ parser.add_argument('--insolset', '-s', default='sol000', dest='insolset', help=
 parser.add_argument('--insoltab', '-t', default=None, dest='insoltab', help='Input soltab name (e.g. tabin000) - if not given use all')
 parser.add_argument('--outh5parm', '-o', default='output.h5', dest='outh5parm', help='Output h5parm name [default: output.h5]')
 parser.add_argument('--verbose', '-V', dest='verbose', action='store_true', help='Go Vebose! (default=False)')
-parser.add_argument('--concataxis', '-c', dest='concataxis', help='Axis to concatenate (e.g. time)')
-parser.add_argument('--resampaxes', '-r', nargs='*', dest='resampaxes', default=['time','freq'], help='Axes to resanple (default: time,freq)') # nargs=* means 0 or more
-parser.add_argument('--fillaxes', '-f', nargs='*', dest='fillaxes', default=['ant'], help='Axes to fill with flagged data for missing values (default: ant)')
 args = parser.parse_args()
 
-if len(args.h5parmFiles) < 1:
+if len(args.h5parmFiles) < 2:
     parser.print_help()
     sys.exit()
 
@@ -32,69 +29,16 @@ if args.verbose: _logging.setLevel("debug")
 
 ################################
 
-class Soltabr(Soltab):
-    def __init__(self, soltab, useCache = False, args = {}):
-        Soltab.__init__(self, soltab, useCache = False, args = {})
-
-    def resample(self, axisValsNew, axisName, flag=False):
-        """
-        Get a list of axis values for an axisName and resample with neearest interpolation the values of the table
-        axisValsNew : new sampling values of the axis
-        axisName : name of the axis to resample
-        flag: if True the expanded values are flagged
-        """
-        logging.info('Resampling: %s...' % axisName)
-        axisIdx = self.getAxesNames().index(axisName)
-        from scipy.interpolate import interp1d
-
-        # get axis values and update them
-        axisVals = self.getAxisValues(axisName)
-        self.axes[axisName] = axisValsNew
-
-        # transform str axis into numbers, this is needed e.g. for antennas
-        if isinstance(axisValsNew[0], str):
-            axisValsNew = [int(codecs.encode(s, 'hex'), 16) for s in axisValsNew]
-            axisVals = [int(codecs.encode(s, 'hex'), 16) for s in axisVals]
-
-        self.obj.val = interp1d(axisVals, self.obj.val, axis=axisIdx, kind='nearest', fill_value='extrapolate')(axisValsNew)
-        self.obj.weight = interp1d(axisVals, self.obj.weight, axis=axisIdx, kind='nearest', fill_value='extrapolate')(axisValsNew)
-
-        # flag all resampled data
-        if flag:
-            # move relevant axis at the end
-            vals = np.swapaxes(self.obj.val, axisIdx, 0)
-            weights = np.swapaxes(self.obj.weight, axisIdx, 0)
-            for i, val in enumerate(axisValsNew):
-                if not val in axisVals:
-                    weights[i] = 0.
-                    vals[i] = np.nan
-            self.obj.val = np.swapaxes(vals, axisIdx, 0)
-            self.obj.weight = np.swapaxes(weights, axisIdx, 0)
-
-        self.setSelection() # reset selection to empty since values are different
-
-def equalArr(arr):
-    """
-    Check if arrays are equal
-    """
-    for a in arr[1:]:
-        if not np.array_equal(a, arr[0]):
-            return False
-    return True
-
 # check input
 if len(args.h5parmFiles) == 1 and ',' in args.h5parmFiles[0]:
     # Treat input as a string with comma-separated values
     args.h5parmFiles = args.h5parmFiles[0].strip('[]').split(',')
     args.h5parmFiles  = [f.strip() for f in args.h5parmFiles]
 
-if args.resampaxes is None: args.resampaxes = []
-if args.fillaxes is None: args.fillaxes = []
-
 # read all tables
 insolset = args.insolset
 if args.insoltab is None:
-    # use all soltabs
+    # use all soltabs, find out names from first h5parm
     h5 = h5parm(args.h5parmFiles[0], readonly=True)
     solset = h5.getSolset(insolset)
     insoltabs = solset.getSoltabNames()
@@ -107,6 +51,7 @@ h5s = []
 for h5parmFile in args.h5parmFiles:
     h5 = h5parm(h5parmFile, readonly=True)
     h5s.append(h5)
+
 # open output
 h5Out = h5parm(args.outh5parm, readonly = False)
 
@@ -117,7 +62,7 @@ for insoltab in insoltabs:
 
     for h5 in h5s:
         solset = h5.getSolset(insolset)
-        soltab = Soltabr(solset.obj._f_get_child(insoltab)) # use inherited class
+        soltab = solset.getSoltab(insoltab)
         soltabs.append( soltab )
         # collect pointings
         sous = solset.getSou()
@@ -128,95 +73,38 @@ for insoltab in insoltabs:
         [antennaNames.append(k) for k in ants.keys() if not k in antennaNames]
         [antennaPositions.append(v) for v in ants.values() if not any((v == x).all() for x in antennaPositions)]
 
-    # combine tables
-    logging.info('Ordering data...')
-    resamp = {axis:[] for axis in args.fillaxes+args.resampaxes} # dict to store+compute resamp/fill axis values
-    for soltab in soltabs:
-        if soltab.getAxesNames() != soltabs[0].getAxesNames():
-            logging.critical('Input soltabs have different axes.')
-            sys.exit(1)
-        if soltab.getType() != soltabs[0].getType():
-            logging.critical('Input soltabs have different types.')
-            sys.exit(1)
-        # concatenate axis values inside the resamp dict
-        for axis in soltab.getAxesNames():
-            if axis in resamp.keys():
-                resamp[axis].append(soltab.getAxisValues(axis))
-
+    # create output axes
+    logging.info("Sorting output axes...")
     axes = soltabs[0].getAxesNames()
     typ = soltabs[0].getType()
-
-    if not args.concataxis in axes:
-        logging.critical('Axis %s not found.' % args.concataxis)
-        sys.exit(1)
-
-    # all arrays in each entry of the resamp dict are combined in a single one
-    # this array has all values, not repeated and sorted
-    for axis, allAxisVals in iter(resamp.items()):
-        resamp[axis] = np.array(sorted(list(set(chain(*allAxisVals)))))
+    allAxesVals = {axis:[] for axis in axes}
+    allShape = []
+    for axis in axes:
         print('len %s:' % axis, end='')
-        for axisVals in allAxisVals:
-            print(' %i' % len(axisVals), end='')
-        print('Will be: %i' % len(resamp[axis]))
+        for soltab in soltabs:
+            allAxesVals[axis].append( soltab.getAxisValues(axis) )
+            print(' %i' % soltab.getAxisLen(axis), end='')
+        allAxesVals[axis] = np.array(sorted(list(set(chain(*allAxesVals[axis])))))
+        allShape.append(len(allAxesVals[axis]))
+        print(' - Will be: %i' % len(allAxesVals[axis]))
 
-    # resampling/filling of values
+    # make final arrays
+    logging.info("Allocating space...")
+    logging.debug("Shape:"+str(allShape))
+    allVals = np.empty( shape=allShape )
+    allVals[:] = np.nan
+    allWeights = np.zeros( shape=allShape )
+
+    # fill arrays
+    logging.info("Filling new table...")
     for soltab in soltabs:
-        for axis in soltab.getAxesNames():
-            if axis in resamp.keys():
-                if axis in args.resampaxes and len(resamp[axis]) != len(soltab.getAxisValues(axis)) and args.concataxis != axis:
-                    soltab.resample(resamp[axis], axis, flag=False)
-                elif axis in args.fillaxes and len(resamp[axis]) != len(soltab.getAxisValues(axis)) and args.concataxis != axis:
-                    soltab.resample(resamp[axis], axis, flag=True)
+        coords = []
+        for axis in axes:
+            coords.append( np.searchsorted( allAxesVals[axis], soltab.getAxisValues(axis) ) )
+        allVals[np.ix_(*coords)] = soltab.obj.val
+        allWeights[np.ix_(*coords)] = soltab.obj.weight
 
-    # sort tables based on the first value of the concatAxis
-    logging.info('Sorting tables...')
-    firstValsConcatAxis = [soltab.getAxisValues(args.concataxis)[0] for soltab in soltabs]
-    idxToSort = [i[0] for i in sorted(enumerate(firstValsConcatAxis), key=lambda x:x[1])]
-    #print 'idxToSort', idxToSort
-    soltabs = [soltabs[i] for i in idxToSort]
-
-    # re-order dirs (if concatenating in directions)
-    if args.concataxis == 'dir':
-        pointingNames = [pointingNames[i] for i in idxToSort]
-        pointingDirections = [pointingDirections[i] for i in idxToSort]
-
-    # get all vals/weights to concatenate
-    logging.info('Allocate final data...')
-    valsAll = []; weightsAll = []
-    for soltab in soltabs:
-        valsAll.append( soltab.obj.val )
-        weightsAll.append( soltab.obj.weight )
-
-    # creating concatenated table
-    logging.info('Concatenate final dataset...')
-    axesVals = []; vals = []; weights = []
-    for a, axis in enumerate(axes):
-        axisValsAll = []
-        for i, soltab in enumerate(soltabs):
-            val = soltab.getAxisValues(axis)
-            # allow concatenating directions all named 'pointing'
-            if axis == 'dir' and len(val) == 1 and val[0] == 'pointing':
-                axisValsAll.append( np.array(['pointing-%03i' % i]) )
-                pointingNames[i] = 'pointing-%03i' % i
-            else:
-                axisValsAll.append( val )
-
-        # check if all elements of the axis are equal
-        if equalArr(axisValsAll):
-            axesVals.append(axisValsAll[0])
-
-        elif axis == args.concataxis:
-            # check intersection is empty
-            if len( set.intersection(*map(set,axisValsAll)) ) > 0:
-                logging.critical('Intersection of axis values in %s is not empty.' % axis)
-            axesVals.append( np.array( [item for sublist in axisValsAll for item in sublist] ) ) # flatten list of lists
-            vals = np.concatenate(valsAll, axis=a)
-            weights = np.concatenate(weightsAll, axis=a)
-
-        else:
-            # TODO: check missing axis values (e.g. missing antenna) and add flagged data
-            logging.critical('Axis %s is not the same for all h5parms.' % axis)
-            sys.exit(1)
+    # TODO: interpolate on selected axes
 
     logging.info('Writing output...')
     solsetOutName = args.insolset
@@ -229,10 +117,9 @@ for insoltab in insoltabs:
         solsetOut = h5Out.makeSolset(solsetOutName)
 
     # create soltab
-    weights[ np.isnan(vals) ] = 0. # to be sure, can be removed when DPPP does it properly
-    logging.debug( "Set weight to 0 for %i values." % np.sum(np.isnan(vals)) )
     solsetOut.makeSoltab(typ, soltabOutName, axesNames=axes, \
-                     axesVals=axesVals, vals=vals, weights=weights)
+                     axesVals=[ allAxesVals[axis] for axis in axes ], \
+                     vals=allVals, weights=allWeights)
 
 sourceTable = solsetOut.obj._f_get_child('source')
 antennaTable = solsetOut.obj._f_get_child('antenna')
