7f55721b7660011d16739825c8c18b8d09235693
revoltek <rev@voo.it>
2018-01-11 15:15:17 +0000

    offset

--- losoto/operations/polalign.py
+++ losoto/operations/polalign.py
@@ -9,13 +9,14 @@ logging.debug('Loading POLALIGN module.')
 def _run_parser(soltab, parser, step):
     soltabOut = parser.getstr( step, 'soltabOut', 'phasediff' )
     maxResidual = parser.getfloat( step, 'maxResidual', 1. )
-    smooth = parser.getint( step, 'smooth', 0 )
+    fitOffset = parser.getbool( step, 'fitOffset', False )
+    average = parser.getbool( step, 'average', False )
     replace = parser.getbool( step, 'replace', False )
     refAnt = parser.getstr( step, 'refAnt', '' )
-    return run(soltab, soltabOut, maxResidual, smooth, replace, refAnt)
+    return run(soltab, soltabOut, maxResidual, fitOffset, average, replace, refAnt)
 
 
-def run( soltab, soltabOut='phasediff', maxResidual=1., smooth=0, replace=False, refAnt='' ):
+def run( soltab, soltabOut='phasediff', maxResidual=1., fitOffset=False, average=False, replace=False, refAnt='' ):
     """
     Estimate polarization alignment as delay
 
@@ -27,11 +28,14 @@ def run( soltab, soltabOut='phasediff', maxResidual=1., smooth=0, replace=False,
     maxResidual : float, optional
         Maximum acceptable rms of the residuals in radians before flagging, by default 1. If 0: No check.
 
-    smooth : int, optional
-        Median-smooth in time the resulting delays, number of timestamp for smoothing window. By default 0 (no smooth)    
+    fitOffset : bool, optional
+        Assume that together with a delay each station has also a differential phase offset (important for old LBA observations). By default False.    
+
+    average : bool, optional
+        Mean-average in time the resulting delays/offset. By default False.    
     
     replace : bool, optional
-        replace using smoothed value instead of flag bad data? Smooth must be active. By default, False
+        replace using smoothed value instead of flag bad data? Smooth must be active. By default, False.
         
     refAnt : str, optional
         Reference antenna, by default the first.
@@ -45,11 +49,6 @@ def run( soltab, soltabOut='phasediff', maxResidual=1., smooth=0, replace=False,
 
     delaycomplex = lambda d, freq, y: abs(np.cos(d[0]*freq)  - np.cos(y)) + abs(np.sin(d[0]*freq)  - np.sin(y))
     #delaycomplex = lambda d, freq, y: abs(d[0]*freq  - y)
-
-    # input check
-    if smooth != 0 and smooth % 2 == 0:
-        logging.warning('Smooth should be odd, adding 1.')
-        smooth += 1
 
     solType = soltab.getType()
     if solType != 'phase':
@@ -83,8 +82,6 @@ def run( soltab, soltabOut='phasediff', maxResidual=1., smooth=0, replace=False,
         vals = reorderAxes( vals, soltab.getAxesNames(), ['pol','freq','time'] )
         weights = reorderAxes( weights, soltab.getAxesNames(), ['pol','freq','time'] )
 
-        fitdelayguess = -1.e-9 # good guess, do not use 0 as it seems the minimizer is unstable with that
-
         if 'RR' in coord['pol'] and 'LL' in coord['pol']:
             coord1 = np.where(coord['pol'] == 'RR')[0][0]
             coord2 = np.where(coord['pol'] == 'LL')[0][0]
@@ -97,12 +94,11 @@ def run( soltab, soltabOut='phasediff', maxResidual=1., smooth=0, replace=False,
             weights[:] = 0
         else:
 
-            fit_delays=[]
-            fit_weights=[]
+            fit_delays=[]; fit_offset=[]; fit_weights=[]
             for t, time in enumerate(times):
 
                 # apply flags
-                idx       = ( (weights[coord1,:,t] != 0.) & (weights[coord2,:,t] != 0.) & (coord['freq'] > 40.e6) )
+                idx       = ( (weights[coord1,:,t] != 0.) & (weights[coord2,:,t] != 0.) & (coord['freq'] > 40.e6) & (coord['freq'] < 70.e6) )
                 freq      = np.copy(coord['freq'])[idx]
                 phase1    = vals[coord1,:,t][idx]
                 phase2    = vals[coord2,:,t][idx]
@@ -113,35 +109,34 @@ def run( soltab, soltabOut='phasediff', maxResidual=1., smooth=0, replace=False,
                     logging.debug('Not enough unflagged point for the timeslot '+str(t))
                     continue
     
-                # if more than 1/4 of chans are flagged
+                # if more than 1/2 of chans are flagged
                 if (len(idx) - len(freq))/float(len(idx)) > 1/2.:
                     logging.debug('High number of filtered out data points for the timeslot %i: %i/%i' % (t, len(idx) - len(freq), len(idx)) )
     
-                phase_diff = (phase1 - phase2)
+                phase_diff = phase1 - phase2
                 phase_diff = np.mod(phase_diff + np.pi, 2.*np.pi) - np.pi
                 phase_diff = np.unwrap(phase_diff)
 
                 A = np.vstack([freq, np.ones(len(freq))]).T
                 fitresultdelay = np.linalg.lstsq(A, phase_diff.T)[0]
                 # get the closest n*(2pi) to the intercept and refit with only 1 parameter
-                numjumps = np.around(fitresultdelay[1]/(2*np.pi))
-                A = np.reshape(freq, (-1,1)) # no b
-                phase_diff -= numjumps * 2 * np.pi
-                fitresultdelay = np.linalg.lstsq(A, phase_diff.T)[0][0]
+                if not fitOffset:
+                    numjumps = np.around(fitresultdelay[1]/(2*np.pi))
+                    A = np.reshape(freq, (-1,1)) # no b
+                    phase_diff -= numjumps * 2 * np.pi
+                    fitresultdelay = np.linalg.lstsq(A, phase_diff.T)[0]
+                    fitresultdelay = [fitresultdelay[0],0.] # set offset to 0 to keep the rest of the script equal
 
-                #fitresultdelay, success = scipy.optimize.leastsq(delaycomplex, [fitdelayguess], args=(freq, phase_diff))
                 # fractional residual
-                residual = np.mean(np.abs( fitresultdelay*freq-phase_diff ))
+                residual = np.mean(np.abs( fitresultdelay[0]*freq + fitresultdelay[1] - phase_diff ))
 
-                #print "t:", t, "result:", fitresultdelay, "residual:", residual
-
-                fit_delays.append(fitresultdelay)
+                fit_delays.append(fitresultdelay[0])
+                fit_offset.append(fitresultdelay[1])
                 if maxResidual == 0 or residual < maxResidual:
-                    fitdelayguess = fitresultdelay
                     fit_weights.append(1.)
                 else:       
                     # high residual, flag
-                    logging.warning('Bad solution for ant: '+coord['ant']+' (time: '+str(t)+', resdiaul: '+str(residual)+').')
+                    logging.warning('Bad solution for ant: '+coord['ant']+' (time: '+str(t)+', residual: '+str(residual)+') -> ignoring.')
                     fit_weights.append(0.)
 
                 # Debug plot
@@ -149,7 +144,6 @@ def run( soltab, soltabOut='phasediff', maxResidual=1., smooth=0, replace=False,
                 if doplot and t%100==0 and coord['ant'] == 'RS310LBA':
                     if not 'matplotlib' in sys.modules:
                         import matplotlib as mpl
-                        mpl.rc('font',size =8 )
                         mpl.rc('figure.subplot',left=0.05, bottom=0.05, right=0.95, top=0.95,wspace=0.22, hspace=0.22 )
                         mpl.use("Agg")
                     import matplotlib.pyplot as plt
@@ -159,16 +153,15 @@ def run( soltab, soltabOut='phasediff', maxResidual=1., smooth=0, replace=False,
                     ax = fig.add_subplot(111)
 
                     # plot rm fit
-                    plotdelay = lambda delay, freq: np.mod( delay*freq + np.pi, 2.*np.pi) - np.pi
-                    #ax.plot(freq, plotdelay(fitresultdelay[0], freq), "-", color='purple')
-                    ax.plot(freq, fitresultdelay*freq, "-", color='purple')
+                    plotdelay = lambda delay, offset, freq: np.mod( delay*freq + offset + np.pi, 2.*np.pi) - np.pi
+                    ax.plot(freq, fitresultdelay[0]*freq + fitresultdelay[1], "-", color='purple')
 
                     ax.plot(freq, np.mod(phase1 + np.pi, 2.*np.pi) - np.pi, 'ob' )
                     ax.plot(freq, np.mod(phase2 + np.pi, 2.*np.pi) - np.pi, 'og' )
                     #ax.plot(freq, np.mod(phase_diff + np.pi, 2.*np.pi) - np.pi, '.', color='purple' )                           
                     ax.plot(freq, phase_diff, '.', color='purple' )                           
  
-                    residual = np.mod(plotdelay(fitresultdelay, freq)-phase_diff + np.pi,2.*np.pi)-np.pi
+                    residual = np.mod(plotdelay(fitresultdelay[0], fitresultdelay[1], freq)-phase_diff + np.pi,2.*np.pi)-np.pi
                     ax.plot(freq, residual, '.', color='yellow')
     
                     ax.set_xlabel('freq')
@@ -182,24 +175,32 @@ def run( soltab, soltabOut='phasediff', maxResidual=1., smooth=0, replace=False,
 
             fit_weights = np.array(fit_weights)
             fit_delays = np.array(fit_delays)
+            fit_offset = np.array(fit_offset)
 
-            # smooth
-            if smooth != 0:
+            # avg in time
+            if average:
+                print fit_delays, fit_offset, fit_weights
                 fit_delays_bkp = fit_delays[ fit_weights == 0 ]
-                np.putmask(fit_delays, fit_weights==0, np.nan)
-                fit_delays = generic_filter(fit_delays, np.nanmedian, size=smooth, mode='constant', cval=np.nan)
+                fit_offset_bkp = fit_offset[ fit_weights == 0 ]
+                np.putmask(fit_delays, fit_weights == 0, np.nan)
+                np.putmask(fit_offset, fit_weights == 0, np.nan)
+                fit_delays[:] = np.nanmean(fit_delays)
+                fit_offset[:] = np.nanmean(fit_offset)
+
                 if replace:
                     fit_weights[ fit_weights == 0 ] = 1.
                     fit_weights[ np.isnan(fit_delays) ] = 0. # all the size was flagged cannot estrapolate value
                 else:
                     fit_delays[ fit_weights == 0 ] = fit_delays_bkp
+                    fit_offset[ fit_weights == 0 ] = fit_offset_bkp
 
-            logging.debug('%s: average delay: %f ns' % ( coord['ant'], np.mean(fit_delays)*1e9))
+            logging.debug('%s: average delay: %f ns (offset: %f)' % ( coord['ant'], np.mean(fit_delays)*1e9, np.mean(fit_offset)))
             for t, time in enumerate(times):
                 #vals[:,:,t] = 0.
                 #vals[coord1,:,t] = fit_delays[t]*np.array(coord['freq'])/2.
                 vals[coord1,:,t] = 0
-                vals[coord2,:,t] = -1.*(fit_delays[t]*np.array(coord['freq']))#/2.
+                phase = np.mod(fit_delays[t]*coord['freq'] + fit_offset[t] + np.pi, 2.*np.pi) - np.pi
+                vals[coord2,:,t] = -1.*phase#/2.
                 #weights[:,:,t] = 0.
                 weights[coord1,:,t] = fit_weights[t]
                 weights[coord2,:,t] = fit_weights[t]
