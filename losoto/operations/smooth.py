ef7cc60e7d5ace9c0bad88e11d9092095a0b36a0
Francesco Gasperin <rev@voo.it>
2018-02-09 13:58:06 +0100

    speed up and handle phases

--- losoto/operations/smooth.py
+++ losoto/operations/smooth.py
@@ -16,7 +16,7 @@ def _run_parser(soltab, parser, step):
 
 def run( soltab, axesToSmooth, size=[], mode='runningmedian', degree=1, replace=False):
     """
-    This operation for LoSoTo implement a smoothing function: running-median on an arbitrary number of axes, running polyfit on one axis, or set all solutions to mean/median value.
+    A smoothing function: running-median on an arbitrary number of axes, running polyfit on one axis, or set all solutions to the mean/median value.
     WEIGHH: flag ready.
 
     Parameters
@@ -48,6 +48,10 @@ def run( soltab, axesToSmooth, size=[], mode='runningmedian', degree=1, replace=
         logging.error("Axes and size lenghts must be 1 for runningpoly.")
         return 1
 
+    if mode == "runningpoly" and soltab.getType() == 'phase':
+        logging.error("Runningpoly mode cannot work on phases.")
+        return 1
+
     for i, s in enumerate(size):
         if s % 2 == 0:
             logging.warning('Size should be odd, adding 1.')
@@ -61,62 +65,89 @@ def run( soltab, axesToSmooth, size=[], mode='runningmedian', degree=1, replace=
             del size[i]
             logging.warning('Axis \"'+axis+'\" not found. Ignoring.')
 
-    for vals, weights, coord, selection in soltab.getValuesIter(returnAxes=axesToSmooth, weight=True):
+    if mode == 'median' or mode == 'mean':
+        vals = soltab.getValues(retAxesVals=False)
+        weights = soltab.getValues(retAxesVals=False, weight=True)
+        np.putmask(vals, weights==0, np.nan)
+        idx_axes = [soltab.getAxesNames().index(axisToSmooth) for axisToSmooth in axesToSmooth]
 
-        # skip completely flagged selections
-        if (weights == 0).all(): continue
+        # handle phases by using a complex array
+        if soltab.getType() == 'phase':
+            vals = np.exp(1j*vals)
 
-        if mode == 'runningmedian':
-            vals_bkp = vals[ weights == 0 ]
-            np.putmask(vals, weights==0, np.nan)
-            valsnew = generic_filter(vals, np.nanmedian, size=size, mode='constant', cval=np.nan)
-            if replace: 
-                weights[ weights == 0] = 1
-                weights[ np.isnan(valsnew) ] = 0 # all the size was flagged cannoth estrapolate value
+        if mode == 'median':
+            vals[:] = np.nanmedian( vals, axis=idx_axes, keepdims=True)
+        if mode == 'mean':
+            vals[:] = np.nanmean( vals, axis=idx_axes, keepdims=True)
+
+        # go back to phases
+        if soltab.getType() == 'phase':
+            vals = np.angle(vals)
+
+        # write back
+        soltab.setValues(vals)
+        if replace:
+            weights[ (weights == 0) ] = 1
+            weights[ np.isnan(vals) ] = 0 # all the slice was flagged, cannot estrapolate value
+            soltab.setValues(weights, weight=True)
+
+    else:
+        for vals, weights, coord, selection in soltab.getValuesIter(returnAxes=axesToSmooth, weight=True):
+    
+            # skip completely flagged selections
+            if (weights == 0).all(): continue
+    
+            if mode == 'runningmedian':
+                # handle phases by using a complex array
+                if soltab.getType() == 'phase':
+                    vals = np.exp(1j*vals)
+
+                vals_bkp = vals[ weights == 0 ]
+                np.putmask(vals, weights==0, np.nan)
+                valsnew = generic_filter(vals, np.nanmedian, size=size, mode='constant', cval=np.nan)
+
+                # go back to phases
+                if soltab.getType() == 'phase':
+                    valsnew = np.angle(valsnew)
+
+                if replace: 
+                    weights[ weights == 0] = 1
+                    weights[ np.isnan(valsnew) ] = 0 # all the size was flagged cannoth estrapolate value
+                else:
+                    valsnew[ weights == 0 ] = vals_bkp
+    
+            elif mode == 'runningpoly':
+                def polyfit(data):
+                    if (np.isnan(data)).all(): return np.nan # all size is flagged
+                    x = np.arange(len(data))[ ~np.isnan(data)]
+                    y = data[ ~np.isnan(data) ]
+                    p = np.polynomial.polynomial.polyfit(x, y, deg=degree)
+                    #import matplotlib as mpl
+                    #mpl.use("Agg")
+                    #import matplotlib.pyplot as plt
+                    #plt.plot(x, y, 'ro')
+                    #plt.plot(x, np.polyval( p[::-1], x ), 'k-')
+                    #plt.savefig('test.png')
+                    #sys.exit()
+                    return np.polyval( p[::-1], (size[0]-1)/2 ) # polyval has opposite convention for polynomial order
+    
+                # flags and at edges pass 0 and then remove them
+                vals_bkp = vals[ weights == 0 ]
+                np.putmask(vals, weights==0, np.nan)
+                valsnew = generic_filter(vals, polyfit, size=size[0], mode='constant', cval=np.nan)
+                if replace:
+                    weights[ weights == 0] = 1
+                    weights[ np.isnan(valsnew) ] = 0 # all the size was flagged cannoth estrapolate value
+                else:
+                    valsnew[ weights == 0 ] = vals_bkp
+                #print coord['ant'], vals, valsnew
+    
             else:
-                valsnew[ weights == 0 ] = vals_bkp
-
-        elif mode == 'runningpoly':
-            def polyfit(data):
-                if (np.isnan(data)).all(): return np.nan # all size is flagged
-                x = np.arange(len(data))[ ~np.isnan(data)]
-                y = data[ ~np.isnan(data) ]
-                p = np.polynomial.polynomial.polyfit(x, y, deg=degree)
-                #import matplotlib as mpl
-                #mpl.use("Agg")
-                #import matplotlib.pyplot as plt
-                #plt.plot(x, y, 'ro')
-                #plt.plot(x, np.polyval( p[::-1], x ), 'k-')
-                #plt.savefig('test.png')
-                #sys.exit()
-                return np.polyval( p[::-1], (size[0]-1)/2 ) # polyval has opposite convention for polynomial order
-
-            # flags and at edges pass 0 and then remove them
-            vals_bkp = vals[ weights == 0 ]
-            np.putmask(vals, weights==0, np.nan)
-            valsnew = generic_filter(vals, polyfit, size=size[0], mode='constant', cval=np.nan)
-            if replace:
-                weights[ weights == 0] = 1
-                weights[ np.isnan(valsnew) ] = 0 # all the size was flagged cannoth estrapolate value
-            else:
-                valsnew[ weights == 0 ] = vals_bkp
-            #print coord['ant'], vals, valsnew
-
-        # TODO: if phase use angular mean/median
-        elif mode == 'median':
-            valsnew = np.nanmedian( vals[(weights!=0)] )
-            if replace: weights[ weights == 0] = 1
-
-        elif mode == 'mean':
-            valsnew = np.nanmean( vals[(weights!=0)] )
-            if replace: weights[ weights == 0] = 1
-
-        else:
-            logging.error('Mode must be: runningmedian, runningpoly, median or mean')
-            return 1
-
-        soltab.setValues(valsnew, selection)
-        if replace: soltab.setValues(weights, selection, weight=True)
+                logging.error('Mode must be: runningmedian, runningpoly, median or mean')
+                return 1
+    
+            soltab.setValues(valsnew, selection)
+            if replace: soltab.setValues(weights, selection, weight=True)
 
     soltab.flush()
     soltab.addHistory('SMOOTH (over %s with mode = %s)' % (axesToSmooth, mode))
