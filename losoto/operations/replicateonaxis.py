05f39767e46425eb488fb88b1b68f6650bbabaaa
revoltek <rev@voo.it>
2019-02-15 14:14:49 +0000

    dd operation

---
+++ losoto/operations/replicateonaxis.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import logging
+from losoto.lib_operations import *
+import numpy as np
+
+logging.debug('Loading REPLICATEONAXIS module.')
+
+def _run_parser(soltab, parser, step):
+    axisReplicate = parser.getstr( step, 'axisReplicate' ) # no default
+    fromCell = parser.getstr( step, 'fromCell', '' )
+
+    parser.checkSpelling( step, soltab, ['axisReplicate', 'fromCell'])
+    return run(soltab, axisReplicate, fromCell)
+
+def run( soltab, axisReplicate, fromCell):
+    """
+    Replace the values along a certain axis taking them from one specific axic cell
+
+    Parameters
+    ----------
+    axisReplicate : str
+        Axis along which replicate the values.
+
+    fromCell : str
+        A cell value in axisReplicate from which to copy the data values.
+    """
+
+    if not axisReplicate in soltab.getAxesNames():
+        logging.error('Cannot find axis %s.' % axisReplicate)
+        return 1
+
+    axisType = type(soltab.getAxisValues(axisReplicate)[0])
+    try:
+        fromCell = np.array([fromCell]).astype(axisType)[0]
+    except:
+        logging.error('Cannot convert to type %s the value in fromCell: %s.' % (str(axisType),fromCell))
+        return 1
+
+    if not fromCell in soltab.getAxisValues(axisReplicate):
+        logging.error('Cannot find %s in %s.' % (fromCell, axisReplicate))
+        return 1
+
+    logging.info("Replicate axis on soltab: "+soltab.name)
+
+    # get the cell to replicate
+    soltab.setSelection(**{axisReplicate:fromCell})
+    vals = soltab.getValues(retAxesVals=False)
+    weights = soltab.getValues(retAxesVals=False, weight=True)
+
+    cellPos = list(soltab.getAxisValues(axisReplicate)).index(fromCell)
+    axisReplicateLen = soltab.getAxisLen(axisReplicate, ignoreSelection=True)
+    axisReplicatePos = soltab.getAxesNames().index(axisReplicate)
+
+    # expand on the right axis
+    vals = np.repeat(vals, repeats=axisReplicateLen, axis=axisReplicatePos)
+
+    # write back
+    soltab.setSelection()
+    soltab.setValues(vals)
+    soltab.setValues(weights, weight=True)
+
+    soltab.addHistory('REPLICATEONAXIS (over axis %s)' % (axisReplicate))
+    return 0
