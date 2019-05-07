6b7807d1a3ceb91b9b74a0e90ded43496fe5ed63
Francesco Gasperin fdg@hs.uni-hamburg.de <baq1889@node31.lofar.gpu.cluster>
2019-05-07 14:23:54 +0200

    tecjump

--- losoto/operations/tecjump.py
+++ losoto/operations/tecjump.py
@@ -11,11 +11,25 @@ logging.debug('Loading TECJUMP module.')
 
 def _run_parser(soltab, parser, step):
     refAnt = parser.getstr( step, 'refAnt', '' )
+    soltabError = parser.getstr( step, 'errorTab', '' )
 
-    parser.checkSpelling( step, soltab, ['refAnt'])
-    return run(soltab, refAnt)
+    parser.checkSpelling( step, soltab, ['refAnt', 'soltabError'])
+    return run(soltab, refAnt, soltabError)
 
-def run( soltab, refAnt='' ):
+def run( soltab, refAnt='', soltabError='' ):
+    """
+    Remove jumps from TEC solutions.
+    WEIGHT: uses the errors.
+
+    Parameters
+    ----------
+    soltabError : str, optional
+        The table name with solution errors. By default it has the same name of soltab with "error" in place of "tec".
+
+    refAnt : str, optional
+        Reference antenna for phases. By default None.
+
+    """
 
     import scipy.ndimage.filters
     import numpy as np
@@ -31,20 +45,6 @@ def run( soltab, refAnt='' ):
         """
         return np.std(data[np.where(np.abs(data) < sigma * np.std(data))])
 
-    def mask_interp(vals, mask, method='nearest'):
-        """
-        return interpolated values for masked elements
-        """
-        this_vals = vals.copy()
-        #this_vals[mask] = np.interp(np.where(mask)[0], np.where(~mask)[0], vals[~mask])
-        #this_vals[mask] = griddata(np.where(~mask)[0], vals[~mask], np.where(mask)[0], method)
-
-        # griddata has nan bug with nearest, I need to use vq
-        code, dist = vq.vq(np.where(mask)[0], np.where(~mask)[0])
-        this_vals[ np.where(mask)[0] ] = this_vals[code]
-        
-        return this_vals
-
     def getPhaseWrapBase(freqs):
         """
         freqs: frequency grid of the data
@@ -58,11 +58,93 @@ def run( soltab, refAnt='' ):
         steps = np.dot(np.dot(np.linalg.inv(np.dot(A.T, A)), A.T), 2 * np.pi * np.ones((nF, ), dtype=np.float))
         return steps
 
-    tec_jump_val = abs(getPhaseWrapBase([42.308e6,42.308e6+23828e6])[0])*1.2
-    print ('TEC jump:',tec_jump_val,'TECU')
-    #tec_jump_val = 0.048
-    #maxsize = 300
-    #clip = 10 # TECs over these amount of jumps are flagged
+    class Block(object):
+        """
+        Implement blocks of contiguous series of good datapoints
+        """
+        def __init__(self, jump_idx_init, jump_idx_end, vals, vals_e, tec_jump):
+            self.tec_jump = tec_jump
+            self.jump_idx_init = jump_idx_init
+            self.jump_idx_end = jump_idx_end
+            self.idx = range(jump_idx_init, jump_idx_end)
+            self.idx_exp = range(jump_idx_init-10, jump_idx_end+10)
+            self.vals = vals
+            self.vals_e = vals_e
+            self.len = len(vals)
+            self.expand()
+
+        def get_vals(self, idx):
+            """
+            Return values and errors.
+            Idx is already the local index of the block.
+            """
+            return self.vals_exp[idx], self.vals_e_exp[idx]
+
+        def expand(self):
+            """
+            predict values+err outside the edges
+            TODO: for now it's just nearest
+            """
+            self.vals_exp = np.concatenate( \
+                    ( np.full((10), self.vals[0]) ,
+                      self.vals,\
+                      np.full((10), self.vals[-1])
+                    ) )
+            # errors are between 0-1
+            self.vals_e_exp = np.concatenate( \
+                    ( np.linspace(0.1,1,10) ,
+                      self.vals_e,\
+                      np.linspace(0.1,1,10)
+                    ) )
+
+        def jump(self, jump=0):
+            """
+            Return values+errors of this block after applying a jump
+            """
+            self.vals_exp += self.tec_jump*jump
+
+    
+    def distance(block1, block2):
+        """
+        Estimate a distance between two blocks taking advantage of blocks predicted edges
+        """
+        # find indexes of vals_exp that are common to both blocks
+        common_idx_block1 = [i for i, x in enumerate(block1.idx_exp) if x in block2.idx_exp]
+        common_idx_block2 = [i for i, x in enumerate(block2.idx_exp) if x in block1.idx_exp]
+        if len(common_idx_block1) == 0: return 0 # no overlapping
+
+        v1, v1_e = block1.get_vals(idx=common_idx_block1)
+        v2, v2_e = block2.get_vals(idx=common_idx_block2)
+
+        num = np.sum( v1_e * v2_e * np.abs(v1 - v2))
+        den = np.sum( v1_e * v2_e )
+
+        return num/den
+
+    def global_dispersion(blocks):
+        """
+        Calculate a "dispersion" of the time serie summing up the distances between blocks
+        """
+        dispersion = 0
+        for block1, block2 in zip(blocks[:-1], blocks[1:]):
+            dispersion += distance(block1, block2)
+
+        return dispersion
+
+
+    if soltab.getType() != 'tec':
+        logging.error('TECJUMP works only on tec solutions.')
+        return 1
+
+    if soltabError == '': soltabError = soltab.name.replace('tec','error')
+    solset = soltab.getSolset()
+    soltab_e = solset.getSoltab(soltabError)
+    try:
+        seltab_e = solset.getSoltab(soltabError)
+    except:
+        logging.error('Cannot fine error solution table %s.' % soltabError)
+        return 1
+    vals_e_all = soltab_e.getValues( retAxesVals = False, weight = False )
 
     logging.info("Removing TEC jumps from soltab: "+soltab.name)
 
@@ -72,8 +154,18 @@ def run( soltab, refAnt='' ):
         refAnt = ants[0]
     if refAnt == '': refAnt = None
 
+    # Get the theoretical tec jump
+    tec_jump_theory = abs(getPhaseWrapBase([42.308e6,42.308e6+23828e6])[0])
 
-    # TODO: check if it's a Tec table
+    # Find the average jump on all antennas by averaging all jumps found to be withing 0.5 and 2 times the tec_jump_theory
+    vals = soltab.getValues( retAxesVals=False, reference=refAnt )
+    timeAxis = soltab.getAxesNames().index('time')
+    vals = np.swapaxes(vals, 0, timeAxis)
+    vals = vals[1,...] - vals[:-1,...] 
+    vals = vals[(vals > tec_jump_theory*1) & (vals < tec_jump_theory*1.5)]
+    tec_jump = np.median(vals)
+
+    logging.info('TEC jump - theoretical: %.5f TECU - estimated: %.5f TECU' % (tec_jump_theory, tec_jump))
 
     for vals, weights, coord, selection in soltab.getValuesIter(returnAxes='time', weight = True, reference=refAnt):
 
@@ -82,87 +174,74 @@ def run( soltab, refAnt='' ):
         # skip reference
         if (vals[(weights != 0)] == 0).all(): continue
 
-        # kill large values
-#        weights[abs(vals/tec_jump_val)>clip] = 0
+        logging.info('Working on ant: %s' % (coord['ant']))
 
-        # interpolate flagged values to get resonable distances
-#        vals = mask_interp(vals, mask=(weights == 0))/tec_jump_val
-        # add edges to allow intervals to the borders
-#        vals = np.insert(vals, 0, vals[0])
-#        vals = np.insert(vals, len(vals), vals[-1])
-        #print(vals[:30],vals.shape, coord['dir'])
-        vals = np.fmod(vals, tec_jump_val)
+        vals_init = np.copy( vals ) # backup for final check
+        vals_e = np.squeeze(vals_e_all[selection])
 
-        #print(vals[:30],vals.shape)
+        # TODO: remove very large jumps (>1 or <-1)
 
-#        def find_jumps(d_vals):
-#            # jump poistion finder
-#            d_smooth = scipy.ndimage.filters.median_filter( mask_interp(d_vals, mask=(abs(d_vals)>0.8)), 21 )
-#            d_vals -= d_smooth
-#            jumps = list(np.where(np.abs(d_vals) > 1.)[0])
-#            return [0]+jumps+[len(d_vals)-1] # add edges
-#
-#        class Jump(object):
-#            def __init__(self, jumps_idx, med):
-#                self.idx_left = jumps_idx[0]
-#                self.idx_right = jumps_idx[1]
-#                self.jump_left = np.rint(d_vals[self.idx_left])
-#                self.jump_right = np.rint(d_vals[self.idx_right])
-#                self.size = self.idx_right-self.idx_left
-#                self.hight = np.median(vals[self.idx_left+1:self.idx_right+1]-med)
-#                if abs((self.hight-self.jump_left)-med) > abs((self.hight-self.jump_right)-med):
-#                    self.closejump = self.jump_right
-#                else:
-#                    self.closejump = self.jump_left
-#
-#        i = 0
-#        while i<len(coord['time']):
-#            # get tec[i] - tec[i+1], i.e. the derivative assuming constant timesteps
-#            # this is in units of tec_jump_val!
-#            d_vals = np.diff(vals)
-#            # get jumps idx, idx=n means a jump beteen val n and n+1
-#            jumps_idx = find_jumps(d_vals)
-#
-#            # get regions
-#            med = np.median(vals)
-#            jumps = [Jump(jump_idx, med) for jump_idx in zip( jumps_idx[:-1], jumps_idx[1:] )]
-#            jumps = [jump for jump in jumps if jump.closejump != 0]
-#            jumps = [jump for jump in jumps if jump.size != 0] # prevent bug on edges
-#            jumps = [jump for jump in jumps if jump.size < maxsize]
-#
-#            jumps.sort(key=lambda x: (np.abs(x.size), x.hight), reverse=False) #smallest first
-#            #print [(j.hight, j.closejump) for j in jumps]
-#
-#            plot = False
-#            if plot:
-#                import matplotlib.pyplot as plt
-#                fig, ((ax1, ax2, ax3)) = plt.subplots(3, 1, sharex=True)
-#                fig.subplots_adjust(hspace=0)
-#                d_smooth = scipy.ndimage.filters.median_filter( mask_interp(d_vals, mask=(abs(d_vals)>0.8)), 31 )
-#                ax1.plot(d_vals,'k-')
-#                ax2.plot(d_smooth,'k-')
-#                ax3.plot(vals, 'k-')
-#                [ax3.axvline(jump_idx+0.5, color='r', ls=':') for jump_idx in jumps_idx]
-#                ax1.set_ylabel('d_vals')
-#                ax2.set_ylabel('d_vals - smooth')
-#                ax3.set_ylabel('TEC/jump')
-#                ax3.set_xlabel('timestep')
-#                ax1.set_xlim(xmin=-10, xmax=len(d_smooth)+10)
-#                fig.savefig('plots/%stecjump_debug_%03i' % (coord['ant'], i))
-#            i+=1
-#
-#            if len(jumps) == 0: 
-#                break
-#
-#            # move down the highest to the side closest to the median
-#            j = jumps[0]
-#            #print j.idx_left, j.idx_right, j.jump_left, j.jump_right, j.hight, j.closejump
-#
-#            vals[j.idx_left+1:j.idx_right+1] -= j.closejump
-#            logging.debug("%s: Number of jumps left: %i - Removing jump: %i - Size %i" % (coord['ant'], len(jumps_idx)-2, j.closejump, j.size))
-            
-        # re-create proper vals
-#        vals = vals[1:-1]*tec_jump_val
+        i = 0
+        while True:
+
+            # find blocks
+            vals_diff = np.diff(vals)
+            vals_diff = np.concatenate(([100], list(vals_diff), [100]))
+            jumps_idx = np.where(np.abs(vals_diff) > tec_jump/2.)[0]
+
+            # no more jumps
+            if len(jumps_idx) == 2: break
+
+            # add edges and make blocks
+            #jumps_idx = [0]+list(jumps_idx)+[len(vals)]
+            blocks = []
+            for jump_idx_init, jump_idx_end in zip(jumps_idx[:-1],jumps_idx[1:]):
+                blocks.append( Block(jump_idx_init, jump_idx_end, \
+                        vals[jump_idx_init:jump_idx_end],  vals_e[jump_idx_init:jump_idx_end], tec_jump=tec_jump) )
+    
+            # cycle on blocks and merge 
+            dispersions = []
+            for block in blocks:
+                block.jump(-1)
+                dispersions.append( global_dispersion(blocks) )
+                block.jump(+1) # return to normality
+
+                block.jump(+1)
+                dispersions.append( global_dispersion(blocks) )
+                block.jump(-1) # return to normality
+            #print "dispersions:", dispersions
+                
+            # find best jump
+            idx = dispersions.index( min(dispersions) )
+            if idx%2 == 0: best_jump = -1
+            else: best_jump = +1
+            best_block = blocks[idx//2]
+            logging.debug('(Cycle %i - #jumps: %i) - Best jump (%i) on block: %i (len %i)' % (i, len(jumps_idx), best_jump, idx//2, best_block.len) )
+                
+            # recreate vals with the updated block value
+            vals[best_block.idx] = best_block.vals + tec_jump*best_jump
+
+            plot = False
+            if plot:
+                import matplotlib as mpl
+                mpl.use("Agg")
+                import matplotlib.pyplot as plt
+                plt.plot(coord['time'], vals_init, 'k.')
+                plt.plot(coord['time'], vals, 'r.')
+                plt.savefig('test%03i.png' % i)
+                plt.clf()
+            i+=1
+
+        # check that this is the closest value to the global minimum
+        # (this assumes that the majority of the points are correct)
+        distances = []; jumps = []
+        for jump in range(-5,5):
+            distances.append( np.sum( np.abs(vals_init - (vals + tec_jump*jump) ) ) )
+            jumps.append(jump)
+        idx = distances.index( min(distances) )
+        print('Rescaling all values by %i jumps.' % (jumps[idx]) )
+        vals += tec_jump*jumps[idx]
+
         # set back to 0 the values for flagged data
         vals[weights == 0] = 0
         soltab.setValues(vals, selection)
