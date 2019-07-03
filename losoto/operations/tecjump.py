12bf37dd5e9dec2d25f1a2360b3c24b27a070db0
Francesco Gasperin fdg@hs.uni-hamburg.de <baq1889@node31.lofar.gpu.cluster>
2019-07-03 19:56:41 +0200

    update

--- losoto/operations/tecjump.py
+++ losoto/operations/tecjump.py
@@ -12,46 +12,14 @@ logging.debug('Loading TECJUMP module.')
 def _run_parser(soltab, parser, step):
     refAnt = parser.getstr( step, 'refAnt', '' )
     soltabError = parser.getstr( step, 'errorTab', '' )
+    ncpu = parser.getint( '_global', 'ncpu', 0 )
 
     parser.checkSpelling( step, soltab, ['refAnt', 'soltabError'])
-    return run(soltab, refAnt, soltabError)
+    return run(soltab, refAnt, soltabError, ncpu)
 
-def run( soltab, refAnt='', soltabError='' ):
-    """
-    Remove jumps from TEC solutions.
-    WEIGHT: uses the errors.
 
-    Parameters
-    ----------
-    soltabError : str, optional
-        The table name with solution errors. By default it has the same name of soltab with "error" in place of "tec".
-
-    refAnt : str, optional
-        Reference antenna for phases. By default None.
-
-    """
-
-    import scipy.ndimage.filters
-    import numpy as np
-    from scipy.optimize import minimize
+def _run_antenna(vals, vals_e, vals_init, weights, selection, tec_jump, antname, outQueue):
     import itertools
-    from scipy.interpolate import griddata
-    import scipy.cluster.vq as vq
-    import itertools
-
-    def getPhaseWrapBase(freqs):
-        """
-        freqs: frequency grid of the data
-        return the step size from a local minima (2pi phase wrap) to the others [0]: TEC, [1]: clock
-        """
-        freqs = np.array(freqs)
-        nF = freqs.shape[0]
-        A = np.zeros((nF, 2), dtype=np.float)
-        A[:, 1] = freqs * 2 * np.pi * 1e-9
-        A[:, 0] = -8.44797245e9 / freqs
-        steps = np.dot(np.dot(np.linalg.inv(np.dot(A.T, A)), A.T), 2 * np.pi * np.ones((nF, ), dtype=np.float))
-        return steps
-
 
     class Block(object):
         """
@@ -184,7 +152,131 @@ def run( soltab, refAnt='', soltabError='' ):
                     
         return potentials
 
+    ###############################################################
+    
+    i = 0
+    while i<500:
 
+        # find blocks
+        vals_diff = np.diff(vals)
+        vals_diff = np.concatenate(([100], list(vals_diff), [100]))
+        jumps_idx = np.where(np.abs(vals_diff) > tec_jump*(2/3.))[0]
+
+        # no more jumps
+        if len(jumps_idx) == 2: break
+
+        # add edges and make blocks
+        blocks = []
+        for jump_idx_init, jump_idx_end in zip(jumps_idx[:-1],jumps_idx[1:]):
+            blocks.append( Block(jump_idx_init, jump_idx_end, \
+                    vals[jump_idx_init:jump_idx_end],  vals_e[jump_idx_init:jump_idx_end], tec_jump=tec_jump) )
+
+        # cycle on blocks and merge 
+        potentials = []
+        for j, block in enumerate(blocks):
+            block.jump(-1)
+            potentials.append( global_potential(blocks, range(len(vals_init)) ) )
+            block.jump(+1) # return to normality
+            # decrease potential for larger blocks to favour smaller block movements
+            potentials[-1] /= (block.len)**(1/4.)
+            #print(j, potentials[-1], block.len)
+
+            block.jump(+1)
+            potentials.append( global_potential(blocks, range(len(vals_init)) ) )
+            block.jump(-1) # return to normality
+            # decrease potentials for larger blocks to favour smaller block movements
+            potentials[-1] /= (block.len)**(1/4.)
+            #print(j, potentials[-1], block.len)
+
+            # prevent moving if the block is already at the minimum
+            #potentials0 = global_potential(blocks, range(len(vals_init)) )
+            #print(j, potentials0, block.len)
+            #if potentials0 < potentials[-1] and  potentials0 < potentials[-2]:
+            #     potentials[-1] = 1e10
+            #     potentials[-2] = 1e10
+
+        #print "potentials:", potentials
+            
+        # find best jump
+        idx = potentials.index( max(potentials) )
+        if idx%2 == 0: best_jump = -1
+        else: best_jump = +1
+        best_block = blocks[idx//2]
+        logging.debug('(Ant %s - Cycle %i - #jumps: %i) - Best jump (%i) on block: %i (len %i)' % (antname, i, len(jumps_idx), best_jump, idx//2, best_block.len) )
+        #print idx/2., potentials[idx]
+            
+        # recreate vals with the updated block value
+        vals[best_block.idx] = best_block.vals + tec_jump*best_jump
+
+        plot = False
+        if plot:
+            best_block.vals_exp += tec_jump*best_jump
+            import matplotlib as mpl
+            mpl.use("Agg")
+            import matplotlib.pyplot as plt
+            fig = plt.figure(figsize=(16, 8))
+            ax = fig.add_subplot(111)
+            ax.plot(vals_init, 'k.')
+            for block in blocks:
+                if block is best_block: continue
+                #ax.plot(block.idx_exp, block.vals_exp, 'b,')
+                ax.errorbar(block.idx_exp[-1:], block.vals_exp[-1:], block.vals_e_exp[-1:]/30., color='blue', ecolor='blue', marker=',', linestyle='')
+                ax.errorbar(block.idx_exp[:1], block.vals_exp[:1], block.vals_e_exp[:1]/30., color='blue', ecolor='blue', marker=',', linestyle='')
+            #ax.plot(coord['time'], vals, 'r.')
+            ax.errorbar(range(len(vals)), vals, vals_e/30., color='green', ecolor='red', marker='.', linestyle='')
+            ax.set_xlim(0,len(vals))
+            ax.set_ylim(-0.5,0.5)
+            fig.savefig('jump_%s_%03i.png' % (coord['ant'], i), bbox_inches='tight')
+            fig.clf()
+        i+=1
+
+    # check that this is the closest value to the global minimum
+    # (this assumes that the majority of the points are correct)
+    distances = []; jumps = []
+    for jump in range(-5,5):
+        distances.append( np.sum( np.abs(vals_init - (vals + tec_jump*jump) ) ) )
+        jumps.append(jump)
+    idx = distances.index( min(distances) )
+    logging.info('%s: Rescaling all values by %i jumps.' % (antname, jumps[idx]) )
+    vals += tec_jump*jumps[idx]
+    
+    outQueue.put([vals, weights, selection])
+
+
+
+def run( soltab, refAnt='', soltabError='', ncpu=0 ):
+    """
+    Remove jumps from TEC solutions.
+    WEIGHT: uses the errors.
+
+    Parameters
+    ----------
+    soltabError : str, optional
+        The table name with solution errors. By default it has the same name of soltab with "error" in place of "tec".
+
+    refAnt : str, optional
+        Reference antenna for phases. By default None.
+
+    """
+
+    import scipy.ndimage.filters
+    import numpy as np
+    from scipy.optimize import minimize
+    from scipy.interpolate import griddata
+    import scipy.cluster.vq as vq
+
+    def getPhaseWrapBase(freqs):
+        """
+        freqs: frequency grid of the data
+        return the step size from a local minima (2pi phase wrap) to the others [0]: TEC, [1]: clock
+        """
+        freqs = np.array(freqs)
+        nF = freqs.shape[0]
+        A = np.zeros((nF, 2), dtype=np.float)
+        A[:, 1] = freqs * 2 * np.pi * 1e-9
+        A[:, 0] = -8.44797245e9 / freqs
+        steps = np.dot(np.dot(np.linalg.inv(np.dot(A.T, A)), A.T), 2 * np.pi * np.ones((nF, ), dtype=np.float))
+        return steps
 
     if soltab.getType() != 'tec':
         logging.error('TECJUMP works only on tec solutions.')
@@ -221,6 +313,8 @@ def run( soltab, refAnt='', soltabError='' ):
 
     logging.info('TEC jump - theoretical: %.5f TECU - estimated: %.5f TECU' % (tec_jump_theory, tec_jump))
 
+    mpm = multiprocManager(ncpu, _run_antenna)
+
     for vals, weights, coord, selection in soltab.getValuesIter(returnAxes='time', weight = True, reference=refAnt):
 
         # skip all flagged
@@ -237,97 +331,13 @@ def run( soltab, refAnt='', soltabError='' ):
         vals_e = np.squeeze(vals_e_all[selection])
         vals_e[np.where(weights == 0)] = 1.
 
-        i = 0
-        while i<500:
+        mpm.put([vals, vals_e, vals_init, weights, selection, tec_jump, coord['ant']])
 
-            # find blocks
-            vals_diff = np.diff(vals)
-            vals_diff = np.concatenate(([100], list(vals_diff), [100]))
-            jumps_idx = np.where(np.abs(vals_diff) > tec_jump*(2/3.))[0]
-
-            # no more jumps
-            if len(jumps_idx) == 2: break
-
-            # add edges and make blocks
-            blocks = []
-            for jump_idx_init, jump_idx_end in zip(jumps_idx[:-1],jumps_idx[1:]):
-                blocks.append( Block(jump_idx_init, jump_idx_end, \
-                        vals[jump_idx_init:jump_idx_end],  vals_e[jump_idx_init:jump_idx_end], tec_jump=tec_jump) )
-    
-            # cycle on blocks and merge 
-            potentials = []
-            for j, block in enumerate(blocks):
-                block.jump(-1)
-                potentials.append( global_potential(blocks, range(len(vals_init)) ) )
-                block.jump(+1) # return to normality
-                # decrease potential for larger blocks to favour smaller block movements
-                potentials[-1] /= (block.len)**(1/4.)
-                #print(j, potentials[-1], block.len)
-
-                block.jump(+1)
-                potentials.append( global_potential(blocks, range(len(vals_init)) ) )
-                block.jump(-1) # return to normality
-                # decrease potentials for larger blocks to favour smaller block movements
-                potentials[-1] /= (block.len)**(1/4.)
-                #print(j, potentials[-1], block.len)
-
-                # prevent moving if the block is already at the minimum
-                #potentials0 = global_potential(blocks, range(len(vals_init)) )
-                #print(j, potentials0, block.len)
-                #if potentials0 < potentials[-1] and  potentials0 < potentials[-2]:
-                #     potentials[-1] = 1e10
-                #     potentials[-2] = 1e10
-
-            #print "potentials:", potentials
-                
-            # find best jump
-            idx = potentials.index( max(potentials) )
-            if idx%2 == 0: best_jump = -1
-            else: best_jump = +1
-            best_block = blocks[idx//2]
-            logging.debug('(Cycle %i - #jumps: %i) - Best jump (%i) on block: %i (len %i)' % (i, len(jumps_idx), best_jump, idx//2, best_block.len) )
-            #print idx/2., potentials[idx]
-                
-            # recreate vals with the updated block value
-            vals[best_block.idx] = best_block.vals + tec_jump*best_jump
-
-            plot = False
-            if plot:
-                best_block.vals_exp += tec_jump*best_jump
-                import matplotlib as mpl
-                mpl.use("Agg")
-                import matplotlib.pyplot as plt
-                fig = plt.figure(figsize=(16, 8))
-                ax = fig.add_subplot(111)
-                ax.plot(vals_init, 'k.')
-                for block in blocks:
-                    if block is best_block: continue
-                    #ax.plot(block.idx_exp, block.vals_exp, 'b,')
-                    ax.errorbar(block.idx_exp[-1:], block.vals_exp[-1:], block.vals_e_exp[-1:]/30., color='blue', ecolor='blue', marker=',', linestyle='')
-                    ax.errorbar(block.idx_exp[:1], block.vals_exp[:1], block.vals_e_exp[:1]/30., color='blue', ecolor='blue', marker=',', linestyle='')
-                #ax.plot(coord['time'], vals, 'r.')
-                ax.errorbar(range(len(vals)), vals, vals_e/30., color='green', ecolor='red', marker='.', linestyle='')
-                ax.set_xlim(0,len(vals))
-                ax.set_ylim(-0.5,0.5)
-                fig.savefig('jump_%s_%03i.png' % (coord['ant'], i), bbox_inches='tight')
-                fig.clf()
-            i+=1
-
-        # check that this is the closest value to the global minimum
-        # (this assumes that the majority of the points are correct)
-        distances = []; jumps = []
-        for jump in range(-5,5):
-            distances.append( np.sum( np.abs(vals_init - (vals + tec_jump*jump) ) ) )
-            jumps.append(jump)
-        idx = distances.index( min(distances) )
-        logging.info('Rescaling all values by %i jumps.' % (jumps[idx]) )
-        vals += tec_jump*jumps[idx]
-
+    mpm.wait()
+    for (vals, weights, selection) in mpm.get():
         # set back to 0 the values for flagged data
         vals[weights == 0] = 0
         soltab.setValues(vals, selection)
         soltab.addHistory('TECJUMP')
 
     return 0
-
-
