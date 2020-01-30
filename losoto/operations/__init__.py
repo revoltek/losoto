5e9bb6fa6e4d3045655f617cca3ad867759cdee8
Francesco Gasperin <rev@voo.it>
2020-01-30 17:23:12 +0100

    update

--- losoto/operations/__init__.py
+++ losoto/operations/__init__.py
@@ -1,9 +1,10 @@
 import os, time, glob
+from os.path import dirname, basename, isfile
 
-__all__ = [ os.path.basename(f)[:-3] for f in glob.glob(os.path.dirname(__file__)+"/*.py") if f[0] != '_']
-
-for x in __all__:
-    __import__(x, locals(), globals())
+modules = glob.glob(dirname(__file__)+"/*.py")
+__all__ = [basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py') and not basename(f).startswith('_')]
+del modules
+from . import *
 
 class Timer(object):
     """
