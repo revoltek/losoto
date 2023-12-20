6743cf175097dcf86dcb2d05fa893abb7728f140
Francesco Gasperin fdg@hs.uni-hamburg.de <baq1889@lgc2.lofar.gpu.cluster>
2023-12-20 16:31:03 +0100

    everybeam

--- losoto/operations/lofarbeam.py
+++ losoto/operations/lofarbeam.py
@@ -11,33 +11,41 @@ logging.debug('Loading LOFARBEAM module.')
 # this funct is called by losoto to set parameters and call the real run()
 def _run_parser(soltab, parser, step):
     ms = parser.getstr( step, 'ms') # no default
-    inverse = parser.getbool( step, 'inverse', False)
     useElementResponse = parser.getbool( step, 'useElementResponse', True )
     useArrayFactor = parser.getbool( step, 'useArrayFactor', True )
-    useChanFreq = parser.getbool( step, 'useChanFreq', True )
+    allChan = parser.getbool( step, 'allChan', False)
 
-    parser.checkSpelling( step, soltab, ['ms', 'inverse', 'useElementResponse', 'useArrayFactor', 'useChanFreq'])
-    return run(soltab, ms, inverse, useElementResponse, useArrayFactor, useChanFreq)
+    parser.checkSpelling( step, soltab, ['ms', 'useElementResponse', 'useArrayFactor', 'allChan'])
+    return run(soltab, ms, useElementResponse, useArrayFactor, allChan)
 
-def run( soltab, ms, inverse=False, useElementResponse=True, useArrayFactor=True, useChanFreq=True ):
+def run( soltab, ms, useElementResponse=True, useArrayFactor=True, allChan=False ):
     """
     Generic unspecified step for easy expansion.
 
     Parameters
     ----------
-    opt1 : float
-        Is a mandatory parameter.
+    ms : string
+        A measurement set
+
+    allChan : bool, optional
+        Assume that the frequency axes of the MS and the soltab are the same, faster. By default: False.
 
     """
     # load specific libs
     import numpy as np
-    import casacore.tables as pt
-    from lofar.stationresponse import stationresponse
+    import everybeam as eb
 
-    sr = stationresponse(ms, inverse, useElementResponse, useArrayFactor, useChanFreq)
+    tel = eb.load_telescope(ms, element_response_model='hamaker')#, inverse, useElementResponse, useArrayFactor, useChanFreq)
 
-    numants = pt.taql('select gcount(*) as numants from '+ms+'::ANTENNA').getcol('numants')[0]
+    #import casacore.tables as pt
+    #numants = pt.taql('select gcount(*) as numants from '+ms+'::ANTENNA').getcol('numants')[0]
+    numants = soltab.getAxisLen('ant')
     times = soltab.getAxisValues('time')
+    freqs = soltab.getAxisValues('freq')
+
+    if soltab.getType() != 'amplitude' and soltab.getType() != 'phase':
+        logging.error('Beam prediction work only for amplitude/phase solution tables.')
+        return 1
 
     for vals, coord, selection in soltab.getValuesIter(returnAxes=['ant','time','pol','freq'], weight=False):
         vals = reorderAxes( vals, soltab.getAxesNames(), ['ant','time','freq','pol'])
@@ -45,20 +53,30 @@ def run( soltab, ms, inverse=False, useElementResponse=True, useArrayFactor=True
         for stationnum in range(numants):
             logging.debug('Working on station number %i' % stationnum)
             for itime, time in enumerate(times):
-                beam = sr.evaluateStation(time=time, station=stationnum)
-                # Reshape from [nfreq, 2, 2] to [nfreq, 4]
-                beam = beam.reshape(beam.shape[0], 4)
+                if allChan:
+                    beam = tel.station_response(time=time, station_idx=stationnum)
+                    beam = beam.reshape(beam.shape[0], 4)
+                    if soltab.getAxisLen('pol') == 2:
+                        beam = beam[:,[0,3]] # get only XX and YY
 
-                if soltab.getAxisLen('pol') == 2:
-                    beam = beam[:,[0,3]] # get only XX and YY
-
-                if soltab.getType() == 'amplitude':
-                    vals[stationnum, itime, :, :] = np.abs(beam)
-                elif soltab.getType() == 'phase':
-                    vals[stationnum, itime, :, :] = np.angle(beam)
+                    if soltab.getType() == 'amplitude':
+                        vals[stationnum, itime, :, :] = np.abs(beam)
+                    elif soltab.getType() == 'phase':
+                        vals[stationnum, itime, :, :] = np.angle(beam)
                 else:
-                    logging.error('Beam prediction work only for amplitude/phase solution tables.')
-                    return 1
+                    for ifreq, freq in enumerate(freqs):
+                        beam = tel.station_response(time=time, station_idx=stationnum, freq=freq)
+                        # Reshape from [2, 2] to [4]
+                        beam = beam.reshape(4)
+    
+                        if soltab.getAxisLen('pol') == 2:
+                            beam = beam[[0,3]] # get only XX and YY
+    
+                        if soltab.getType() == 'amplitude':
+                            vals[stationnum, itime, ifreq, :] = np.abs(beam)
+                        elif soltab.getType() == 'phase':
+                            vals[stationnum, itime, ifreq, :] = np.angle(beam)
+
 
         vals = reorderAxes( vals, ['ant','time','freq','pol'], [ax for ax in soltab.getAxesNames() if ax in ['ant','time','freq','pol']] )
         soltab.setValues(vals, selection)
