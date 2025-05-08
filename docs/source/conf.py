39a1bc992561806bdd558ef7d1640dac4da429ef
Marcel Loose <loose@astron.nl>
2025-05-08 16:21:23 +0200

    WIP: Fix configuration file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -75,7 +75,7 @@ release = u'2.5.0'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['*progressbar*']
+exclude_patterns = []
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
@@ -100,7 +100,7 @@ html_theme = 'classic'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+#html_static_path = ['_static']
 
 
 # -- Options for HTMLHelp output ------------------------------------------
