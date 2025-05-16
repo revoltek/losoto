7087cbcb2ac8c34613d48ba0a440ad891fd65720
Marcel Loose <loose@astron.nl>
2025-05-16 15:11:52 +0200

    Clean up code tree
    Removed directory `losoto/tests`. It didn't contain any (working) tests we'd want to keep, except for `losoto_test.py`, which was moved to the `tests` directory, because we want to retain it (at least temporarily).
    Removed some out-dated files in the `tools` directory. Maybe it can go completely, but I'm not sure (yet).

--- tools/lofarinit-para.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# optimized for leiden paracluster
-if [ -z $PYTHONPATH ]; then export $PYTHONPATH="" ; fi
-export PYTHONPATH="/net/lofar1/data1/oonk/rh7_losoto_generic:${PYTHONPATH}"
-export PATH="${PATH}:/net/lofar1/data1/oonk/rh7_losoto_generic/bin"
